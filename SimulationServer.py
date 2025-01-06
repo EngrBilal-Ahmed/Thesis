8da9ea1e3621e7789bbe6f5a79c3c0bb0fde9f38
bilal.ahmed <bilal.ahmed@powersoft19.com>
2025-01-06 21:10:06 +0500

    Code Added

--- SimulationServer.py
+++ SimulationServer.py
@@ -1,5 +1,3 @@
-from cryptography.hazmat.backends import default_backend
-
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
@@ -11,4 +9,4 @@ def authenticate():
     return jsonify({"status": "success", "message": "User authenticated"})
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000)
+    app.run(host="172.19.121.189", port=5000)
